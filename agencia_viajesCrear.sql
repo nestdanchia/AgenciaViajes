698e97e42aeb2e50ab42dd1ef8c06c9f0000e994
nestor daniel <ndchiariello@gmail.com>
2024-07-07 06:44:51 -0300

    Update agencia_viajesCrear.sql
    CREATE DATABASE

--- agencia_viajesCrear.sql
+++ agencia_viajesCrear.sql
@@ -18,7 +18,8 @@
 --
 -- Table structure for table `habitacion`
 --
-
+CREATE DATABASE agencia_viajes;
+use agencia_viajes;
 DROP TABLE IF EXISTS `habitacion`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
